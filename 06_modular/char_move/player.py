73e5b9acd4d6bc8a9f147389170addb9c10b237e
seanrattigan <sean.rattigan@gmail.com>
2021-02-08 10:15:48 +0000

    Added player move by keyboard

--- 06_modular/char_move/player.py
+++ 06_modular/char_move/player.py
@@ -1,12 +1,13 @@
 # @Author:srattigan
 # @Date:2021-02-04 11:29:41
 # @LastModifiedBy:srattigan
-# @Last Modified time:2021-02-04 11:48:03
+# @Last Modified time:2021-02-04 12:54:40
 
 from settings import *
 import arcade
 
 player_img = ":resources:images/animated_characters/female_person/femalePerson_idle.png"
+
 
 class Player(arcade.Sprite):
     def __init__(self, img=player_img):
---
+++ 06_modular/player_move/main_game.py
@@ -0,0 +1,170 @@
+
+"""
+Better Move Sprite With Keyboard
+
+Simple program to show moving a sprite with the keyboard.
+This is slightly better than sprite_move_keyboard.py example
+in how it works, but also slightly more complex.
+
+Artwork from http://kenney.nl
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.sprite_move_keyboard_better
+"""
+
+import arcade
+
+SPRITE_SCALING = 0.5
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Better Move Sprite with Keyboard Example"
+
+MOVEMENT_SPEED = 5
+
+player_img = ":resources:images/animated_characters/female_person/femalePerson_idle.png"
+
+
+class Player(arcade.Sprite):
+    def __init__(self, img=player_img):
+        super().__init__(img, SPRITE_SCALING)
+        self.center_x = 50
+        self.center_y = 50
+        # keys are in order of L, U, R, D
+        self.keys = [arcade.key.LEFT,
+                     arcade.key.UP,
+                     arcade.key.RIGHT,
+                     arcade.key.DOWN]
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        
+
+    def update(self):
+        """ Move the player """
+        # Move player.
+        # Remove these lines if physics engine is moving player.
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Check for out-of-bounds of screen
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer
+        super().__init__(width, height, title)
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+
+        # Track the current state of what key is pressed
+        # THESE WILL BECOME OBSOLETE/UNNECESSARY
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Player()
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites.
+        self.player_list.draw()
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Calculate speed based on the keys pressed
+        self.player_sprite.change_x = 0
+        self.player_sprite.change_y = 0
+
+        if self.up_pressed and not self.down_pressed:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif self.down_pressed and not self.up_pressed:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+        # Call update to move the sprite
+        # If using a physics engine, call update player to rely on physics engine
+        # for movement, and call physics engine here.
+        self.player_list.update()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        for player in self.player_list:
+            if key in player.keys:
+                player.key_handler(key)
+               
+        if key == arcade.key.UP:
+            self.up_pressed = True
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP:
+            self.up_pressed = False
+        elif key == arcade.key.DOWN:
+            self.down_pressed = False
+        elif key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
+
+
+def main():
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
