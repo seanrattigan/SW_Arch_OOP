d7c7afa0c0a3cb19f10cec5ae3c798e0d9a8f459
seanrattigan <sean.rattigan@gmail.com>
2021-03-01 12:12:22 +0000

    Added notes

--- 06_modular/sprite_health/player.py
+++ 06_modular/sprite_health/player.py
@@ -2,7 +2,10 @@
 # the player can only move left and right using the
 # left-arrow and right-arrow keys.
 # the player can shoot using the up arrow
-# give the player a score
+
+# After you get init done, and movement, do this
+# - give the player a score
+# - give the bullet an ID linked to the player
 
 import arcade
 
